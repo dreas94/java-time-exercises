4ef2e633a327622e043a47fccb57cfe387623c90
Andreas Eriksson <dreas94@gmail.com>
2022-04-13 13:14:47 +0200

    Completed Exercise 16

--- src/main/java/se/lexicon/dreas94/java_time_exercises/exercise16/ExerciseMain16.java
+++ src/main/java/se/lexicon/dreas94/java_time_exercises/exercise16/ExerciseMain16.java
@@ -1,9 +1,20 @@
 package se.lexicon.dreas94.java_time_exercises.exercise16;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class ExerciseMain16
 {
     public boolean runExercise()
     {
-        return false;
+        System.out.println("Exercise 16:\nCreate a LocalDateTime object. Then get the LocalDate and LocalTime components into separate objects of respective types from the LocalDateTime object.");
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        LocalDate date = dateTime.toLocalDate();
+        LocalTime time = dateTime.toLocalTime();
+
+        System.out.println("Time: " + time + ", Date: " + date + ", Date + Time: " + dateTime);
+        return true;
     }
 }

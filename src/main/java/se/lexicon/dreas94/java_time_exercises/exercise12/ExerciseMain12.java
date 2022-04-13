2f0f6db370a691a9470d668767021817b7257dca
Andreas Eriksson <dreas94@gmail.com>
2022-04-13 12:59:40 +0200

    Completed Exercise12

--- src/main/java/se/lexicon/dreas94/java_time_exercises/exercise12/ExerciseMain12.java
+++ src/main/java/se/lexicon/dreas94/java_time_exercises/exercise12/ExerciseMain12.java
@@ -1,9 +1,17 @@
 package se.lexicon.dreas94.java_time_exercises.exercise12;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class ExerciseMain12
 {
     public boolean runExercise()
     {
-        return false;
+        System.out.println("Exercise 12:\nUsing DateTimeFormatter format LocalTime from current time and print it out as following pattern: 10:32:53.");
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String text = time.format(formatter);
+        System.out.println(text);
+        return true;
     }
 }

037ef53951dc0d225793335fd4feb2989990bd3b
Andreas Eriksson <dreas94@gmail.com>
2022-04-13 13:14:22 +0200

    Completed Exercise 15

--- src/main/java/se/lexicon/dreas94/java_time_exercises/exercise15/ExerciseMain15.java
+++ src/main/java/se/lexicon/dreas94/java_time_exercises/exercise15/ExerciseMain15.java
@@ -1,9 +1,20 @@
 package se.lexicon.dreas94.java_time_exercises.exercise15;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class ExerciseMain15
 {
     public boolean runExercise()
     {
-        return false;
+        System.out.println("Exercise 15:\nCreate a LocalDateTime object by combining LocalDate object and LocalTime object.");
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        System.out.println("Time: " + time + ", Date: " + date + ", Date + Time: " + dateTime);
+        return true;
     }
 }
